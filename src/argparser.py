aa37bf77c164cebec01d76a45b6c4b8dd9060f08
YuSawan <sawada.yuya.sr7@is.naist.jp>
2025-05-06 00:44:21 +0900

    add scripts for running

---
+++ src/argparser.py
@@ -0,0 +1,71 @@
+import os
+from argparse import ArgumentParser, Namespace
+from dataclasses import dataclass, replace
+from typing import Optional
+
+import yaml
+from transformers import HfArgumentParser, TrainingArguments
+
+
+def load_config_as_namespace(config_file: str | os.PathLike) -> Namespace:
+    with open(config_file, "r") as f:
+        config_dict = yaml.safe_load(f)
+    return Namespace(**config_dict)
+
+
+@dataclass
+class DatasetArguments:
+    """Dataset arguments."""
+    train_file : str
+    validation_file : str
+    test_file: str
+    format: str
+    pretokenize: bool
+
+
+@dataclass
+class ModelArguments:
+    """Model arguments."""
+    model_name: str
+    context_length: int
+    pooler: str
+    freeze_bert: bool
+    lstm_layers: int
+    lstm_hidden_size: int
+    no_crf: bool
+    classifier_dropout: Optional[float]
+    weight_O: float
+    bias_O: Optional[float]
+    cache_dir: Optional[str]
+    prev_path: Optional[str]
+
+
+def parse_args() -> tuple[DatasetArguments, ModelArguments, TrainingArguments]:
+    parser = ArgumentParser()
+    hfparser = HfArgumentParser(TrainingArguments)
+
+    parser.add_argument(
+        "--config_file", metavar="FILE", required=True
+    )
+    parser.add_argument(
+        '--format', type=str, default=None,
+    )
+    parser.add_argument(
+        '--prev_path', metavar="DIR", default=None
+    )
+
+    args, extras = parser.parse_known_args()
+    config = vars(load_config_as_namespace(args.config_file))
+    training_args = hfparser.parse_args_into_dataclasses(extras)[0]
+
+    data_config = config.pop("dataset")
+    model_config = config.pop("model")
+
+    arguments = DatasetArguments(**data_config)
+    model_args = ModelArguments(**model_config)
+    training_args = replace(training_args, **config)
+
+    arguments.format = args.format if args.format else arguments.format
+    model_args.prev_path = args.prev_path if args.prev_path else model_args.prev_path
+
+    return arguments, model_args, training_args
---
+++ src/config.py
@@ -0,0 +1,39 @@
+from typing import Optional
+
+from transformers import PretrainedConfig
+from transformers.models.auto import CONFIG_MAPPING
+
+
+class BertNERConfig(PretrainedConfig):
+    model_type = "bertner"
+    is_composition = True
+    def __init__(self,
+            model_name: str = "google-bert/bert-base-uncased",
+            encoder_config: Optional[dict | PretrainedConfig] = None,
+            pooler: str = 'last',
+            freeze_bert: bool = False,
+            lstm_layers: Optional[int] = None,
+            lstm_hidden_size: Optional[int] = None,
+            classifier_dropout: Optional[float] = None,
+            no_crf: bool = False,
+            weight_O: float = 0.01,
+            bias_O: Optional[float] = None,
+            **kwargs: str
+        ) -> None:
+        super().__init__(**kwargs)
+        if isinstance(encoder_config, dict):
+            encoder_config["model_type"] = (encoder_config["model_type"] if "model_type" in encoder_config else "google-bert/bert-base-uncased")
+            encoder_config = CONFIG_MAPPING[encoder_config["model_type"]](**encoder_config)
+        self.encoder_config = encoder_config
+
+        self.model_name = model_name
+        self.pooler = pooler
+        self.freeze_bert = freeze_bert
+        self.lstm_layers = lstm_layers
+        self.lstm_hidden_size = lstm_hidden_size
+        self.classifier_dropout = classifier_dropout
+        self.no_crf = no_crf
+        self.weight_O = weight_O
+        self.bias_O = bias_O
+
+CONFIG_MAPPING.update({"bertner": BertNERConfig})
